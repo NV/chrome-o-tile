4d3f07f1b2472dcd84a8c3d89cb267a72eb4aa72
Nikita Vasilyev <me@elv1s.ru>
2010-05-19 01:36:57 +0400

    debounce resizing

--- chrome/chrome-o-tile.js
+++ chrome/chrome-o-tile.js
@@ -1,9 +1,26 @@
+// Simplified version of http://code.google.com/p/jquery-debounce/
+Function.prototype.debounce = function debounce(timeout, invokeAsap) {
+	var fn = this, timer;
+	return function debounced() {
+		var args = arguments;
+		var context = this;
+		invokeAsap && !timer && fn.apply(context, args);
+		clearTimeout(timer);
+		timer = setTimeout(function waiting() {
+			!invokeAsap && fn.apply(context, args);
+			timer = null;
+		}, timeout);
+	};
+};
+
 var prevWidth = outerWidth;
 
-window.addEventListener('resize', function resized(e) {
+function resized(e) {
 	if (prevWidth === outerWidth) {
 		return;
 	}
 	chrome.extension.sendRequest({method: 'update'});
 	prevWidth = outerWidth;
-}, false);
+}
+
+window.addEventListener('resize', resized.debounce(100), false);
